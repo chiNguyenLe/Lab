392cfaafe1cab479058958e23f1309417e6adc2a
Chi Nguyen Le <Chi Nguyen Le>
2017-02-15 18:35:46 +0700

    Ver 1.1

---
+++ src/lab/JavaCourseLab.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author ChiNguyen
+ */
+public class JavaCourseLab {
+    private static char sex;
+ 
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println(0b10 + 0b11);
+        // Check if a number is add or even
+        int a = new Scanner(System.in).nextInt();
+        if((a & 1) == 1) System.out.println("The number is odd");
+        if((a & 1) == 0) System.out.println("The number is even");
+    }
+    
+    
+}
+
+
